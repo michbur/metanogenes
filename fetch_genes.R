5fb9db5862568b6083fcea5753706a2942a39094
michbur <michalburdukiewicz@gmail.com>
2017-01-28 22:23:18 +0100

    added gene fetching

---
+++ fetch_genes.R
@@ -0,0 +1,17 @@
+library(rentrez)
+library(dplyr)
+
+raw_dat <- readLines("export_data_all_110117.csv") %>% 
+  strsplit(split = "| ", fixed = TRUE) %>% 
+  do.call(rbind, .)
+
+dat <- raw_dat[-1, ]
+colnames(dat) <- raw_dat[1, ]
+
+
+for(single_name in dat[, "Name"]) {
+  gene_ids <- entrez_search(db = "nucleotide", term = paste0(single_name, "[ORGN]"))
+  
+  genes <- entrez_fetch(db = "nucleotide", id = gene_ids[["ids"]], rettype = "fasta")
+  cat(genes, file = paste0("genes/", single_name, ".fasta"))
+}
