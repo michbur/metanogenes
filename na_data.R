ea9bcb7476756eeb1aada01571a675b06cd94b25
michbur <michalburdukiewicz@gmail.com>
2017-05-24 16:55:46 +0200

    working on doubtful records

--- na_data.R
+++ na_data.R
@@ -46,3 +46,113 @@ for(i in which(num_ids == 0)) {
 #   write.csv(file = "MinimumGrowthRequierments.csv", row.names = FALSE)
 
 levels(dat_df[["MinimumGrowthRequierments"]])[grep("branched", levels(dat_df[["MinimumGrowthRequierments"]]))]
+
+# stringsAsFactors = FALSE) %>% 
+#   mutate(changed = old_name != new_name) %>% 
+#   print(head = FALSE)
+
+doubtful1 <- data.frame(old_name = levels(dat_df[["cellShape"]]),
+                        new_name = c("Cocci", "Coccobacillus", "Irregular cocci",
+                                     "Irregular disc", "Bacillus", "Coccobacillus",
+                                     "Sheathed spiral"))
+attr(doubtful1, "what") <- "cellShape" 
+
+doubtful2 <- data.frame(old_name = levels(dat_df[["creator"]]),
+                        new_name = c("negative", "no data", "no data", "positive", "positive", 
+                                     "unknown (cell lysis)", "variable"))
+attr(doubtful2, "what") <- "creator"
+
+doubtful3 <- data.frame(old_name = levels(dat_df[["gramReaction"]]),
+                        new_name = c("negative", "no data", "no data", "positive", "positive", 
+                                     "unknown (cell lysis)", "variable"))
+attr(doubtful3, "what") <- "gramReaction"
+
+doubtful4 <- data.frame(old_name = levels(dat_df[["maximumcellGcContent"]]),
+                        new_name = c("no data", "no data", "no data", "no data", 
+                                     "present", "no data"))
+attr(doubtful4, "what") <- "maximumcellGcContent"
+
+# Requirements ------------------------------------
+
+# uniformize names - compounds from symbols to full names (B12)
+
+levels(dat_df[["MinimumGrowthRequierments"]]) %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(" and ") %>% unlist %>%
+  strsplit(" or ") %>% unlist %>%
+  strsplit("; ") %>% unlist %>%
+  strsplit("\\+") %>% unlist %>%
+  strsplit("\\\\") %>% unlist %>%
+  strsplit("/") %>% unlist %>%
+  unique %>%
+  sort %>%
+  data.frame(old_name = ., stringsAsFactors = FALSE) %>% 
+  mutate(new_name = c("???", "2-methylbutyrate", "acetate", "acetate", "aminoacids", 
+                      "aminobenzoate", "B12", "biotin", "branched-chain", "Ca", "casamino acids", 
+                      "casamino acids", "casitone", "Co", "CO2", "coenzyme M", "coenzyme M", 
+                      "coenzyme M", "ethanol", "Fe", "H2", "heavy metal solution", "isoleucine", 
+                      "leucine", "MB medium", "methanol", "Mg", "Na2S", "NaCl", "Ni", 
+                      "none", "no data", "nutrient broth", "vitamins", 
+                      "peptone", "peptone", "propionate", "riboflavin", "rumen fluid", 
+                      "rumen fluid", "rumen fuid", "SeO4", "thiamine", "thiamine", 
+                      "trace element solution", 
+                      "trypticase", "trypticase", "trypticase peptone", "trypticase", 
+                      "tryptone", "tungstate", "tungsten", "no data", "vitamins", 
+                      "vitamins", "B12", "yeast extract", "yeast extract")) %>% 
+  mutate(original_record = sapply(old_name, function(ith_name) 
+    as.character(dat_df[["MinimumGrowthRequierments"]][grep(ith_name, as.character(dat_df[["MinimumGrowthRequierments"]]))])[1]
+  )) 
+
+
+levels(dat_df[["AdditionalGrowthRequierments"]]) %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(" and ") %>% unlist %>%
+  strsplit(" or ") %>% unlist %>%
+  strsplit("; ") %>% unlist %>%
+  strsplit("\\+") %>% unlist %>%
+  strsplit("\\\\") %>% unlist %>%
+  strsplit("/") %>% unlist %>%
+  unique %>%
+  sort %>%
+  data.frame(old_name = ., stringsAsFactors = FALSE) %>% 
+  mutate(new_name = c("-1", "acetate", "acetate", "acetate", "acetate", "aminoacids", 
+                      "trimethylamine", "biotin", "C02", "casamino acids", "CO", 
+                      "CoM", "cysteine", "dimethylamine", "yeast extract", "Fe", "Fe2O3", 
+                      "generally organic compounds", 
+                      "H2", "HS-coenzyme M", "methanethiol", "methanol", "methylamine", 
+                      "microelement solution", 
+                      "monomethylamine", "Ni", "none", "none", "none", "no data", "pantolactone", 
+                      "peptone", "peptone ", "K", "rumen fluid", "rumen fluid", 
+                      "selenate", "selenite", "Se", "Se", "tripticase", 
+                      "trypticase", "tungsten", "vitamins", "tryptophan", "yeast extract", "yeast extract", 
+                      "yeast extract")) %>% 
+  mutate(original_record = sapply(old_name, function(ith_name) 
+    as.character(dat_df[["AdditionalGrowthRequierments"]][grep(ith_name, as.character(dat_df[["AdditionalGrowthRequierments"]]))])[1]
+  )) 
+
+
+levels(dat_df[["otherSubstrate"]]) %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(", *") %>% unlist %>%
+  strsplit(" and ") %>% unlist %>%
+  strsplit(" or ") %>% unlist %>%
+  strsplit("; ") %>% unlist %>%
+  strsplit("\\+") %>% unlist %>%
+  strsplit("\\\\") %>% unlist %>%
+  strsplit("/") %>% unlist %>%
+  gsub("not supporting[ ]?:[ ]?", "", x = .) %>% 
+  gsub("not suppoting:", "", x = .) %>% 
+  unique %>%
+  sort %>%
+  data.frame(old_name = ., stringsAsFactors = FALSE) %>% 
+  mutate(new_name = c("1-pentanol", "2", "2-pentanol", "3-butanodiol", "alanine", 
+                        "butyrate", "cyclohexanol", "cyclohexanol", "ethyl acetate", 
+                        "formaldehyde", "fructose", "fumarate", "glucose", "H2", "lactate", 
+                        "leucine", "methanetiol", "methanol", "no data", "pyruvate", 
+                        "theobromine", "theophyline", "trimethoxybenzoate", "valerate")) %>% 
+  mutate(original_record = sapply(old_name, function(ith_name) 
+    as.character(dat_df[["otherSubstrate"]][grep(ith_name, as.character(dat_df[["otherSubstrate"]]))])[1]
+  ))
+
